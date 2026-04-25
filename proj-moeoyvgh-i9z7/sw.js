d58bd62317119cb5db2ff8695e8c33ca3da27401
kele <kele@localhost>
2026-04-26 02:47:32 +0800

    deploy: proj-moeoyvgh-i9z7

---
+++ proj-moeoyvgh-i9z7/sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'kele-game-v1';
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./index.html",
+  "./page.html",
+  "./rules.html",
+  "./records.html",
+  "./data-bridge.js",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg"
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) return response;
+      return fetch(event.request).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
+});
